05d7f72426565b0352f6d9014ea3bdf3abbb4619
Kokindos <kokindoss@gmail.com>
2022-11-15 19:22:48 +0300

    fix choose top bar

--- frontend/lib/presentation/screens/main_screen/topbars/choose_top_bar.dart
+++ frontend/lib/presentation/screens/main_screen/topbars/choose_top_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/presentation/screens/main_screen/topbars/cubit/top_bar_cubit.dart';
+import 'package:frontend/presentation/screens/main_screen/topbars/widgets/choose_point_button.dart';
 import 'package:frontend/presentation/theme/app_colors.dart';
 import 'package:frontend/presentation/theme/app_fonts.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
@@ -27,112 +28,75 @@ class _ChooseTopBarState extends State<ChooseTopBar> {
     return Container(
       height: 60,
       color: AppColors.neutral800,
-      child: Padding(
-        padding: EdgeInsets.all(16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 currentState.isContinueEnabled
                     ? Text(
                         "Вы выбрали точку 1: ${widget.point1.latitude.toStringAsFixed(2)}, "
-                            "${widget.point1.longitude.toStringAsFixed(2)}",
+                        "${widget.point1.longitude.toStringAsFixed(2)}",
                         style:
                             AppFonts.heading3.copyWith(color: AppColors.white),
                       )
                     : Text(
                         "Вы выбрали точку 2: ${widget.point2.latitude.toStringAsFixed(2)}, "
-                            "${widget.point2.longitude.toStringAsFixed(2)}",
+                        "${widget.point2.longitude.toStringAsFixed(2)}",
                         style:
                             AppFonts.heading3.copyWith(color: AppColors.white),
                       ),
               ],
             ),
-            currentState.isContinueEnabled
+          ),
+          Positioned(
+            right: 0,
+            top: 15,
+            child: currentState.isContinueEnabled
                 ? Row(
                     children: [
-                      Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(4),
-                              color: AppColors.white),
-                          child: TextButton(
-                            onPressed: () {
-                              context
-                                  .read<TopBarCubit>()
-                                  .paintChoseAfterSecondPoint(
-                                      widget.point1, widget.point2);
-                            },
-                            child: const Text(
-                              "Продолжить",
-                              style: TextStyle(color: AppColors.gray),
-                            ),
-                          )),
+                      ChoosePointButton(
+                          onPress: () => context
+                              .read<TopBarCubit>()
+                              .paintChoseAfterSecondPoint(
+                                  widget.point1, widget.point2),
+                          title: "Продолжить"),
                       const SizedBox(
                         width: 16,
                       ),
-                      Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(4),
-                              color: AppColors.white),
-                          child: TextButton(
-                            onPressed: () {
-                              context
-                                  .read<TopBarCubit>()
-                                  .returnToPrevious(MainTopBarState());
-                            },
-                            child: const Text(
-                              "Удалить",
-                              style: TextStyle(color: AppColors.gray),
-                            ),
-                          )),
+                      ChoosePointButton(
+                          onPress: () => context
+                              .read<TopBarCubit>()
+                              .returnToPrevious(MainTopBarState()),
+                          title: "Удалить"),
                     ],
                   )
                 : Row(
                     children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            color: AppColors.white),
-                        child: TextButton(
-                          onPressed: () {
-                            context.read<TopBarCubit>().paintBbox();
-                          },
-                          child: const Flexible(
-                            child: Text(
-                              "Начать работу",
-                              style: TextStyle(color: AppColors.gray),
-                            ),
-                          ),
-                        ),
-                      ),
+                      ChoosePointButton(
+                          onPress: () =>
+                              context.read<TopBarCubit>().paintBbox(),
+                          title: "Начать работу"),
                       const SizedBox(
                         width: 16,
                       ),
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            color: AppColors.white),
-                        child: TextButton(
-                          onPressed: () {
-                            context.read<TopBarCubit>().returnToPrevious(
-                                ChooseTopBarState(
-                                    p1: widget.point1,
-                                    p2: widget.point2,
-                                    isBeginEnabled: false,
-                                    isContinueEnabled: true));
-                          },
-                          child: const Text(
-                            "Удалить",
-                            style: TextStyle(color: AppColors.gray),
-                          ),
-                        ),
-                      ),
+                      ChoosePointButton(
+                          onPress: () =>
+                              context.read<TopBarCubit>().returnToPrevious(
+                                    ChooseTopBarState(
+                                        p1: widget.point1,
+                                        p2: widget.point2,
+                                        isBeginEnabled: false,
+                                        isContinueEnabled: true),
+                                  ),
+                          title: "Удалить"),
                     ],
                   ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
